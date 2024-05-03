3a98d9ba14465bba54b1429d6b626d85a9ebd477
BenW2k <benworkman16@gmail.com>
2024-05-03 20:30:13 +0100

    +routine commit additional classes and styles

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,13 +1,24 @@
-import styles from "../styles/NavBar.module.css";
+import styles from "../styles/navBar.module.css";
 import Link from "next/link";
+import Image from "next/image";
 import {NavDropdown} from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
+import darkModeIcon from "../assets/img/moon-dark-theme.svg";
+import darkModeLogo from "../assets/img/figma-logo-white.svg";
 
 export const Navbar = () => {
   return (
-    <div>
-      <Nav className="me-auto">
-        <Link href="/Home">Home</Link>
+    <div className={styles.navbar}>
+      <a href="/">
+        <Image
+          src={darkModeLogo}
+          alt="dark-mode-logo"
+          className={styles["night-main-logo"]}
+        />
+      </a>
+      <Nav className={styles["nav-links"]}>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
         <Link
           href="https://www.linkedin.com/in/ben-workman-ds/"
           target="_blank"
@@ -19,6 +30,18 @@ export const Navbar = () => {
           GitHub
         </Link>
       </Nav>
+      <div className="navbar-icons">
+        <button className={styles["night-mode-button"]}>
+          <Image
+            src={darkModeIcon}
+            alt="dark-mode-toggle-icon"
+            className={styles["night-mode-moon"]}
+          />
+        </button>
+        <button className="nav-button">
+          <span>Contact</span>
+        </button>
+      </div>
     </div>
   );
 };
