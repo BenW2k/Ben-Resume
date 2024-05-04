399cf565b1e513ba5b5429ecee65226b773fba7d
BenW2k <benworkman16@gmail.com>
2024-05-04 02:34:13 +0100

    +Created grid layout + title and subtitle

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,20 @@
+import styles from "./styles/home.module.css";
+import Image from "next/image";
+
 export default function Home() {
-  return <p>Hello world</p>;
+  return (
+    <div className={styles["home-grid"]}>
+      <h1 className={styles["home-title"]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor.{" "}
+      </h1>
+      <p className={styles["home-subtitle"]}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque qui
+        delectus iusto asperiores, sapiente alias fuga eum officia. Praesentium
+        repudiandae non accusamus, explicabo cupiditate quis!
+      </p>
+      <div className="panel-1"></div>
+      <div className="skills-panel"></div>
+    </div>
+  );
 }
