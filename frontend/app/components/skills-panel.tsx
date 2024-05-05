fa7fa955ea34e871214b8724bbdb7613ea245c66
BenW2k <benworkman16@gmail.com>
2024-05-05 04:44:20 +0100

    +skills panel function for creating skill pill blocks

--- frontend/app/components/skills-panel.tsx
+++ frontend/app/components/skills-panel.tsx
@@ -1,16 +1,22 @@
 import styles from "../styles/skillsPanel.module.scss";
 import Image from "next/image";
 
-export default function createTablet(
-  name: string,
-  category: string,
-  imageSource: string
-) {
-  <div className="tablet-container">
-    <Image
-      src={imageSource}
-      alt="skill-icon"
-      className={styles["skill-icon"]}
-    />
-  </div>;
+interface SkillProps {
+  skillName: string;
+  skillCategory: string;
+  imageSource: any;
+}
+export default function CreateSkill(props: SkillProps) {
+  const {skillName, skillCategory, imageSource} = props;
+  return (
+    <div className={styles["tablet-container"]}>
+      <Image
+        src={imageSource}
+        alt="skill-icon"
+        className={styles["skill-icon"]}
+      />
+      <p className={styles["skill-title"]}>{skillName}</p>
+      <p className={styles["skill-category"]}>{skillCategory}</p>
+    </div>
+  );
 }
