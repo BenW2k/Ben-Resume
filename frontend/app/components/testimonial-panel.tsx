b172cb7c12b09af563990ed993cdff32e03b06ba
BenW2k <benworkman16@gmail.com>
2024-05-08 05:48:33 +0100

    +default html structure for elements implemented

--- frontend/app/components/testimonial-panel.tsx
+++ frontend/app/components/testimonial-panel.tsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import styles from "../styles/testimonial.module.css";
+import stock_img from "../assets/img/data-science-light.svg";
 
 export const Testimonial = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <Image
+        src={stock_img}
+        alt="dark-mode-logo"
+        className={styles["test-image"]}
+      />
+      <p className={styles["paragraph-1"]}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      </p>
+      <p className={styles["paragraph-2"]}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        corporis laboriosam magnam quibusdam pariatur
+      </p>
+      <p className={styles["current-element"]}>2/4</p>
+    </div>
+  );
 };
