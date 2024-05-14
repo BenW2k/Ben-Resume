c8fafd28026600288ea8aa1a9fa4364addd766ae
BenW2k <benworkman16@gmail.com>
2024-05-14 15:26:21 +0100

    +updated imports

--- frontend/app/components/testimonial-panel.tsx
+++ frontend/app/components/testimonial-panel.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "../testimonial/styles/testimonial.module.css";
+import styles from "../styles/testimonial/testimonial.module.css";
 import stock_img from "../assets/img/data-science-light.svg";
 
 export const Testimonial = () => {
